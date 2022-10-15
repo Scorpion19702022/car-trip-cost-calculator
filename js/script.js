7e8dc48218355fa42dcaea8da5409734a42a01f3
skorpion <jmosinski@o2.pl>
2022-10-15 21:08:38 +0200

    added first cod in js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const distance = document.querySelector('#distance')
+const fuelUsage = document.querySelector('#fuelusage')
+const fuelCost = document.querySelector('#fuelcost')
+
+const error = document.querySelector('.error')
+
+const btnResult = document.querySelector('.box__btn-result')
+const btnClean = document.querySelector('.box__btn-clean')
+
+console.log(btnResult)
+console.log(btnClean)
